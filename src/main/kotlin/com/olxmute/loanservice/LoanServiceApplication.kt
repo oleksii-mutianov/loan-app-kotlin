ab514adbf413db6a149eb6cbb3ca9fac687fb939
Oleksii_Mutianov <mutianov.o.o@gmail.com>
2020-11-13 18:10:05 +0200

    Initial commit

---
+++ src/main/kotlin/com/olxmute/loanservice/LoanServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.olxmute.loanservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LoanServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<LoanServiceApplication>(*args)
+}
